6b8637c54f0a16f513e3625174422f7b79982d9a
nononorl <86286789+nononorl@users.noreply.github.com>
2021-06-23 13:47:42 +0900

    セクション４_JS基礎まで１

--- src/index.js
+++ src/index.js
@@ -1,10 +1,125 @@
-import "./styles.css";
+// /**
+//  *  const, let等の変数宣言
+//  */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言"
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言"
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "ノノ",
+//   age: 28
+// };
+
+// val4.name = "nonono";
+// val4.address = "Japan"
+// console.log(val4);
+
+// // constで定義したオブジェクトはプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+// /**
+//  * テンプレート文字列
+//  */
+// const name = "ノノ";
+// const age = 28;
+// //「私の名前はノノです。年齢は２８さいです」
+
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// //テンプレート文字列を用いた方法
+// const messaage2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(messaage2);
+
+/**
+ * アロー関数
+ */
+
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// //アロー関数
+// const func2 = (str) => {
+//   return str;
+// }
+// console.log(func2("func2です"));
+
+// const func3 = (num1,num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10,20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "ノノ",
+//   age: 28
+// };
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = [`ノノ`, 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん`);
+// sayHello("ノノ");
+
+/**
+ * スプレット構文
+ */
+
+//配列の展開
+const arr1 = [1, 2];
+console.log(arr1);
+console.log(...arr1);
